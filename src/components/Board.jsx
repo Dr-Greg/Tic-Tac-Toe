be807665e3b6bab191ea4de13e1e77ed9035e0dc
Grigor Mouradyan <gmouradyan@insideboard.com>
2020-08-27 16:06:22 +0200

    feat: fixing board and implementing game components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ squares, onClick }) => {
+const Board = ({ squares, onClick }) => (
   <div className="board">
-    {squares.map((square, index) => {
-      <Square key={i} value={square} onClick={() => onClick(index)} />;
-    })}
-  </div>;
-};
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+  </div>
+);
 
 export default Board;
